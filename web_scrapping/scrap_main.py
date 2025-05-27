35a785aff0dc7d069baffc1bee476d7bd0bb4e7b
PengchC <168306166+PengchC@users.noreply.github.com>
2025-05-27 22:12:09 +0200

    Update scrap_main.py

--- web_scrapping/scrap_main.py
+++ web_scrapping/scrap_main.py
@@ -4,3 +4,6 @@ start_urls = ["https://www.siemens-energy.com/global/en/home/products-services/p
 domain = "https://www.siemens-energy.com"
 
 main(start_urls, domain)
+
+shutil.move("./product-offering", "../RAG/vector_store")
+shutil.move("./file_url_pairs.json", "../RAG/vector_store")
