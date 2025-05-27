82eea71049dce24e5674151a03bf3b7c62e87568
PengchC <168306166+PengchC@users.noreply.github.com>
2025-05-27 21:03:12 +0200

    Create scrap_main.py

---
+++ web_scrapping/scrap_main.py
@@ -0,0 +1,6 @@
+from multiweb_scrapping import main
+
+start_urls = ["https://www.siemens-energy.com/global/en/home/products-services/product-offerings.html"]
+domain = "https://www.siemens-energy.com"
+
+main(start_urls, domain)
